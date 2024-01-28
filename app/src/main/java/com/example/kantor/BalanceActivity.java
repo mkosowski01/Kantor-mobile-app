dfe1ab60f4f68db9ec7ca37b92f2cda3a6a73475
Maciej Kosowski <maciejkosowski01@gmail.com>
2024-01-28 23:21:18 +0100

    add balance view and button

---
+++ app/src/main/java/com/example/kantor/BalanceActivity.java
@@ -0,0 +1,33 @@
+package com.example.kantor;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BalanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balance);
+
+        TextView balanceTextView = findViewById(R.id.balanceTextView);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/kantor/MainActivity.java
+++ app/src/main/java/com/example/kantor/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.kantor;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnMyBalance;
+    private Button btnAddFunds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMyBalance = findViewById(R.id.btnMyBalance);
+        btnMyBalance.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, BalanceActivity.class);
+            startActivity(intent);
+        });
     }
 }
